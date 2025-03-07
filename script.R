65b1d52ffc9987aa8208d84eef57f2b5628ec2b4
tomaszaba <tomas.zaba@outlook.com>
2025-03-07 21:42:02 +0200

    update analysis workflow

--- script.R
+++ script.R
@@ -12,13 +12,16 @@ library(ggplot2)
 ## ---- Read in input data and set Coordinate Reference System -----------------
 source("scripts/read-in-input-data.R")
 
-## ---- Read in Sudan shapefile, project and filter out Al Fasher --------------
-source("scripts/shapefiles.R")
-
 ## ---- Wrangle non-spatial data -----------------------------------------------
 source("scripts/wrangle-data.R")
 
-## ---- Wrangle spatial data -----------------------------------------------
+## ---- Run data plausibility checks -------------------------------------------
+source("scripts/data-quality-check.R")
+
+## ---- Read in Sudan shapefile, project and filter out Al Fasher --------------
+source("scripts/shapefiles.R")
+
+## ---- Wrangle spatial data ---------------------------------------------------
 source("scripts/wrangle-spatial-attributes.R")
 
 ## ---- Run spatial interpolation ----------------------------------------------
