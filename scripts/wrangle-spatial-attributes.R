1a364a2c7ab25dbcd826ec0d200ca6acaf3e13ad
tomaszaba <tomas.zaba@outlook.com>
2025-03-07 21:58:54 +0200

    revise code

--- scripts/wrangle-spatial-attributes.R
+++ scripts/wrangle-spatial-attributes.R
@@ -2,6 +2,8 @@
 #            WORKFLOW TO WRANGLE DATA THROUGH SPATIAL ATTRIBUTES               #
 ################################################################################
 
+
+## ---- Set data as an `sf` object and reproject CRS (WFHZ) --------------------
 wfhz_data <- wfhz_data |> 
   filter(!flag_wfhz == 1) |> 
   select(enumArea, X, Y, gam) |> 
@@ -13,8 +15,9 @@ wfhz_data <- wfhz_data |>
   st_set_crs(value = "EPSG:4326") |> 
   st_transform(crs = "EPSG:32636")
 
-## ---- Summarise XY geocoordinates and cases at cluster level -----------------
-p <- wfhz_data |> 
+## ---- Calculate rates --------------------------------------------------------
+### ---------------------- Raw rates aggregated at the survey cluster level ----
+w <- wfhz_data |> 
   summarise(
     cases = sum(gam, na.rm = TRUE), 
     pop = n(),
@@ -23,16 +26,15 @@ p <- wfhz_data |>
     .by = enumArea
   )
 
-## ---- Compute aspatial (global) empirical bayes smoothed rates ---------------
-p[["smoothed_rates"]] <- eb_rates(p[c("cases", "pop")])[["EB.Rate"]]
-
+### -------------- Compute aspatial (global) empirical bayes smoothed rates ----
+w[["smoothed_rates"]] <- eb_rates(w[c("cases", "pop")])[["EB.Rate"]]
 
 ## ---- Map rates --------------------------------------------------------------
 ### --------------------------------------------------------- Map raw rates ----
 
 #### Create a categorical variable with custom breakpoints ----
-p$rate_category <- cut(
-  p$raw_rates, 
+w$rate_category <- cut(
+  w$raw_rates, 
   breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
   labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
   include.lowest = TRUE
@@ -50,7 +52,7 @@ custom_colors <- c(
 #### Plot ----
 ggplot(data = karamoja) +
   geom_sf(fill = "white") +
-  geom_sf(data = p, aes(color = rate_category)) +
+  geom_sf(data = w, aes(color = rate_category)) +
   scale_color_manual(
     values = custom_colors, 
     name = "Raw Rates" 
@@ -59,8 +61,8 @@ ggplot(data = karamoja) +
 
 ### ---------------------------------------------------- Map smoothed rates ----
 #### Create a categorical variable with custom breakpoints ----
-p$smoothed_rate_category <- cut(
-  p$smoothed_rates, 
+w$smoothed_rate_category <- cut(
+  w$smoothed_rates, 
   breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
   labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
   include.lowest = TRUE
@@ -69,14 +71,14 @@ p$smoothed_rate_category <- cut(
 #### Plot ----
 ggplot(data = karamoja) +
   geom_sf(fill = "white") +
-  geom_sf(data = p, aes(color = smoothed_rate_category)) +
+  geom_sf(data = w, aes(color = smoothed_rate_category)) +
   scale_color_manual(
     values = custom_colors, 
     name = "Smoothed rates"
   ) +
   theme_void()
 
-
+## ---- Set data as an `sf` object and reproject CRS (MUAC) --------------------
 muac_data <- muac_data |> 
   filter(!flag_mfaz == 1) |> 
   select(enumArea, X, Y, gam) |> 
@@ -87,4 +89,67 @@ muac_data <- muac_data |>
   ) |> 
   st_set_crs(value = "EPSG:4326") |> 
   st_transform(crs = "EPSG:32636")
+
+## ---- Calculate rates --------------------------------------------------------
+### ---------------------- Raw rates aggregated at the survey cluster level ----
+m <- muac_data |> 
+  summarise(
+    cases = sum(gam, na.rm = TRUE), 
+    pop = n(),
+    raw_rates = cases / pop,
+    geometry = st_centroid(st_union(geometry)),
+    .by = enumArea
+  )
+
+### -------------- Compute aspatial (global) empirical bayes smoothed rates ----
+m[["smoothed_rates"]] <- eb_rates(m[c("cases", "pop")])[["EB.Rate"]]
+
+## ---- Map rates --------------------------------------------------------------
+### --------------------------------------------------------- Map raw rates ----
+
+#### Create a categorical variable with custom breakpoints ----
+m$rate_category <- cut(
+  m$raw_rates, 
+  breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
+  labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
+  include.lowest = TRUE
+)
+
+#### Color-codes ----
+custom_colors <- c(
+  "<0.05" = "#40E0D0",  
+  "0.05-0.09" = "#DFFF00",
+  "0.10-0.149" = "#FFBF00",
+  "0.15-0.299" = "#FF7F50",
+  "≥0.3" = "#f03b20"
+)
+
+#### Plot ----
+ggplot(data = karamoja) +
+  geom_sf(fill = "white") +
+  geom_sf(data = m, aes(color = rate_category)) +
+  scale_color_manual(
+    values = custom_colors, 
+    name = "Raw Rates" 
+  ) +
+  theme_void()
+
+### ---------------------------------------------------- Map smoothed rates ----
+#### Create a categorical variable with custom breakpoints ----
+m$smoothed_rate_category <- cut(
+  m$smoothed_rates, 
+  breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
+  labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
+  include.lowest = TRUE
+)
+
+#### Plot ----
+ggplot(data = karamoja) +
+  geom_sf(fill = "white") +
+  geom_sf(data = m, aes(color = smoothed_rate_category)) +
+  scale_color_manual(
+    values = custom_colors, 
+    name = "Smoothed rates"
+  ) +
+  theme_void()
 ################################ End of workflow ###############################
