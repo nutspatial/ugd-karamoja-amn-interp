61d4f92de469686562ff918838e8a50ac281b318
tomaszaba <tomas.zaba@outlook.com>
2025-03-10 09:17:22 +0200

    address #7, #8, #9, #10, #13 - WFHZ data

--- scripts/wrangle-spatial-attributes.R
+++ scripts/wrangle-spatial-attributes.R
@@ -1,9 +1,9 @@
 ################################################################################
-#            WORKFLOW TO WRANGLE DATA THROUGH SPATIAL ATTRIBUTES               #
+#             WORKFLOW TO WRANGLE DATA THROUGH SPATIAL ATTRIBUTES              #
 ################################################################################
 
 
-## ---- Set data as an `sf` object and reproject CRS (WFHZ) --------------------
+## ---- Set WFHZ data as an `sf` object  ---------------------------------------
 wfhz_data <- wfhz_data |> 
   filter(!flag_wfhz == 1) |> 
   select(enumArea, X, Y, gam) |> 
@@ -12,42 +12,66 @@ wfhz_data <- wfhz_data |>
     coords = c("X", "Y"),
     dim = "XY"
   ) |> 
-  st_set_crs(value = "EPSG:4326") |> 
-  st_transform(crs = "EPSG:32636")
+  st_set_crs(value = "EPSG:4326")
 
-## ---- Create Voronoi tessellation and plot -----------------------------------
-ts <- wfhz_data |> 
+## ---- Workflow to calculate Spatial Empirical Bayesian Rates (SEBSR) ---------
+dissolved_wfhz_data <- wfhz_data |> 
+  mutate(
+    long_x = st_coordinates(geometry)[, 1], 
+    lati_y = st_coordinates(geometry)[, 2]
+  ) |>  
+  group_by(enumArea) |> 
+  summarise(
+    cases = sum(gam, na.rm = TRUE),
+    pop = n(),
+    X = mean(long_x, na.rm = TRUE),
+    Y = mean(lati_y, na.rm = TRUE)
+  ) |> 
+  as_tibble() |> 
+  select(-geometry) |> 
+  st_as_sf(
+    coords = c("X", "Y"),
+    dim = "XY", 
+    crs = "EPSG:4326"
+  ) |> 
+  st_transform(crs = st_crs(karamoja))
+
+### ---------------------------------------------- Create Thiessen Polygons ----
+voronoi <- dissolved_wfhz_data |> 
   st_union() |> 
   st_voronoi() |> 
   st_collection_extract(type = "POLYGON") |> 
-  st_sf() |> 
+  st_sf()
+
+#### Visualize Thiessen Polygons  ----
+voronoi |> 
   ggplot() +
   geom_sf(fill = NA, color = "grey", linetype = "solid") + 
-  geom_sf(data = wfhz_data, color = "#148F77", size = 1.3) +
+  geom_sf(data = dissolved_wfhz_data, color = "#148F77", size = 1.3) +
   theme_minimal() +
   ggtitle("Thiessen polygons of WFHZ data around Karamoja")
 
+### -------------------------- Calculate spatial weights: K-Near Neighbours ----
+spatial_weights <- dissolved_wfhz_data |> 
+  knearneigh(k = 4, longlat = T) |> 
+  knn2nb(row.names = c[["enumArea"]])
 
-## ---- Calculate rates --------------------------------------------------------
-### ---------------------- Raw rates aggregated at the survey cluster level ----
-w <- wfhz_data |> 
-  summarise(
-    cases = sum(gam, na.rm = TRUE), 
-    pop = n(),
-    raw_rates = cases / pop,
-    geometry = st_centroid(st_union(geometry)),
-    .by = enumArea
-  )
+### ------------------------------------------------------- Calculate rates ----
+sebsr <- EBlocal(
+  ri = c[["cases"]],
+  ni = c[["pop"]],
+  nb = spatial_weights
+)
 
-### -------------- Compute aspatial (global) empirical bayes smoothed rates ----
-w[["smoothed_rates"]] <- eb_rates(w[c("cases", "pop")])[["EB.Rate"]]
+#### Bind data.frames -----
+wrangled_wfhz <- cbind(dissolved_wfhz_data, sebsr)
 
 ## ---- Map rates --------------------------------------------------------------
 ### --------------------------------------------------------- Map raw rates ----
 
 #### Create a categorical variable with custom breakpoints ----
-w$rate_category <- cut(
-  w$raw_rates, 
+wrangled_wfhz$rate_category <- cut(
+  wrangled_wfhz$raw, 
   breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
   labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
   include.lowest = TRUE
@@ -64,18 +88,26 @@ custom_colors <- c(
 
 #### Plot ----
 ggplot(data = karamoja) +
-  geom_sf(fill = "white") +
-  geom_sf(data = w, aes(color = rate_category)) +
+  geom_sf(fill = "white", color = "grey") +
+  geom_sf(data = wrangled_wfhz, aes(color = rate_category)) +
   scale_color_manual(
     values = custom_colors, 
-    name = "Raw Rates" 
+    name = "Raw rates" 
   ) +
-  theme_void()
+  theme_void() +
+  labs(
+    title = "Spatial distribution GAM rates by sampling points across the Karamoja region",
+    subtitle = "Raw rates: cases / total number of children surveyed"
+  ) +
+  theme(
+    plot.title = element_text(size = 11),
+    plot.subtitle = element_text(size = 9, colour = "#706E6D")
+  )
 
 ### ---------------------------------------------------- Map smoothed rates ----
 #### Create a categorical variable with custom breakpoints ----
-w$smoothed_rate_category <- cut(
-  w$smoothed_rates, 
+wrangled_wfhz[["sebsr_cat"]] <- cut(
+  wrangled_wfhz[["est"]], 
   breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
   labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
   include.lowest = TRUE
@@ -83,86 +115,93 @@ w$smoothed_rate_category <- cut(
 
 #### Plot ----
 ggplot(data = karamoja) +
-  geom_sf(fill = "white") +
-  geom_sf(data = w, aes(color = smoothed_rate_category)) +
+  geom_sf(fill = "white", color = "grey") +
+  geom_sf(data = wrangled_wfhz, aes(color = sebsr_cat)) +
   scale_color_manual(
     values = custom_colors, 
     name = "Smoothed rates"
   ) +
-  theme_void()
+  theme_void() + 
+  labs(
+    title = "Spatial distribution of smoothed GAM rates by sampling points across the Karamoja region",
+    subtitle = "Rates smoothed using Spatial Empirical Bayesian"
+  ) +
+  theme(
+    plot.title = element_text(size = 11),
+    plot.subtitle = element_text(size = 9, colour = "#706E6D")
+  )
 
 ## ---- Set data as an `sf` object and reproject CRS (MUAC) --------------------
-muac_data <- muac_data |> 
-  filter(!flag_mfaz == 1) |> 
-  select(enumArea, X, Y, gam) |> 
-  filter(!is.na(X)) |> 
-  st_as_sf(
-    coords = c("X", "Y"),
-    dim = "XY"
-  ) |> 
-  st_set_crs(value = "EPSG:4326") |> 
-  st_transform(crs = "EPSG:32636")
+# muac_data <- muac_data |> 
+#   filter(!flag_mfaz == 1) |> 
+#   select(enumArea, X, Y, gam) |> 
+#   filter(!is.na(X)) |> 
+#   st_as_sf(
+#     coords = c("X", "Y"),
+#     dim = "XY"
+#   ) |> 
+#   st_set_crs(value = "EPSG:4326")
 
-## ---- Calculate rates --------------------------------------------------------
-### ---------------------- Raw rates aggregated at the survey cluster level ----
-m <- muac_data |> 
-  summarise(
-    cases = sum(gam, na.rm = TRUE), 
-    pop = n(),
-    raw_rates = cases / pop,
-    geometry = st_centroid(st_union(geometry)),
-    .by = enumArea
-  )
+# ## ---- Calculate rates --------------------------------------------------------
+# ### ---------------------- Raw rates aggregated at the survey cluster level ----
+# m <- muac_data |> 
+#   summarise(
+#     cases = sum(gam, na.rm = TRUE), 
+#     pop = n(),
+#     raw_rates = cases / pop,
+#     geometry = st_centroid(st_union(geometry)),
+#     .by = enumArea
+#   )
 
-### -------------- Compute aspatial (global) empirical bayes smoothed rates ----
-m[["smoothed_rates"]] <- eb_rates(m[c("cases", "pop")])[["EB.Rate"]]
+# ### -------------- Compute aspatial (global) empirical bayes smoothed rates ----
+# m[["smoothed_rates"]] <- eb_rates(m[c("cases", "pop")])[["EB.Rate"]]
 
-## ---- Map rates --------------------------------------------------------------
-### --------------------------------------------------------- Map raw rates ----
+# ## ---- Map rates --------------------------------------------------------------
+# ### --------------------------------------------------------- Map raw rates ----
 
-#### Create a categorical variable with custom breakpoints ----
-m$rate_category <- cut(
-  m$raw_rates, 
-  breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
-  labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
-  include.lowest = TRUE
-)
+# #### Create a categorical variable with custom breakpoints ----
+# m$rate_category <- cut(
+#   m$raw_rates, 
+#   breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
+#   labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
+#   include.lowest = TRUE
+# )
 
-#### Color-codes ----
-custom_colors <- c(
-  "<0.05" = "#40E0D0",  
-  "0.05-0.09" = "#DFFF00",
-  "0.10-0.149" = "#FFBF00",
-  "0.15-0.299" = "#FF7F50",
-  "≥0.3" = "#f03b20"
-)
+# #### Color-codes ----
+# custom_colors <- c(
+#   "<0.05" = "#40E0D0",  
+#   "0.05-0.09" = "#DFFF00",
+#   "0.10-0.149" = "#FFBF00",
+#   "0.15-0.299" = "#FF7F50",
+#   "≥0.3" = "#f03b20"
+# )
 
-#### Plot ----
-ggplot(data = karamoja) +
-  geom_sf(fill = "white") +
-  geom_sf(data = m, aes(color = rate_category)) +
-  scale_color_manual(
-    values = custom_colors, 
-    name = "Raw Rates" 
-  ) +
-  theme_void()
+# #### Plot ----
+# ggplot(data = karamoja) +
+#   geom_sf(fill = "white") +
+#   geom_sf(data = m, aes(color = rate_category)) +
+#   scale_color_manual(
+#     values = custom_colors, 
+#     name = "Raw Rates" 
+#   ) +
+#   theme_void()
 
-### ---------------------------------------------------- Map smoothed rates ----
-#### Create a categorical variable with custom breakpoints ----
-m$smoothed_rate_category <- cut(
-  m$smoothed_rates, 
-  breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
-  labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
-  include.lowest = TRUE
-)
+# ### ---------------------------------------------------- Map smoothed rates ----
+# #### Create a categorical variable with custom breakpoints ----
+# m$smoothed_rate_category <- cut(
+#   m$smoothed_rates, 
+#   breaks = c(-Inf, 0.05, 0.09, 0.149, 0.299, Inf),
+#   labels = c("<0.05", "0.05-0.09", "0.10-0.149", "0.15-0.299", "≥0.3"),
+#   include.lowest = TRUE
+# )
 
-#### Plot ----
-ggplot(data = karamoja) +
-  geom_sf(fill = "white") +
-  geom_sf(data = m, aes(color = smoothed_rate_category)) +
-  scale_color_manual(
-    values = custom_colors, 
-    name = "Smoothed rates"
-  ) +
-  theme_void()
-################################ End of workflow ###############################
+# #### Plot ----
+# ggplot(data = karamoja) +
+#   geom_sf(fill = "white") +
+#   geom_sf(data = m, aes(color = smoothed_rate_category)) +
+#   scale_color_manual(
+#     values = custom_colors, 
+#     name = "Smoothed rates"
+#   ) +
+#   theme_void()
+# ################################ End of workflow ###############################
