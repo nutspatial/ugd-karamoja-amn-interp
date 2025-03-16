5876e9d73c969b45511f3b9e1cabedc595d84c45
tomaszaba <tomas.zaba@outlook.com>
2025-03-16 20:26:55 +0200

    style code in `download_uga_shp()`

--- R/download-uganda-shapefiles.R
+++ R/download-uganda-shapefiles.R
@@ -1,6 +1,6 @@
 #'
 #' Download zipfile of Uganda shapefiles from Humanitarian Data Exchange
-#' 
+#'
 #' @param url Download URL for Uganda shapefiles from Humanitarian Data
 #'   Exchange. This is set to https://data.humdata.org/dataset/6d6d1495-196b-49d0-86b9-dc9022cde8e7/resource/43dbdff6-aadd-4941-957e-f7eab0717f53/download/uga_admbnda_ubos_20200824_shp.zip
 #'   by default.
@@ -8,33 +8,34 @@
 #'   "data-raw/uganda_shp.zip" by default.
 #' @param overwrite Logical. Should `destfile` be overwritten if present?
 #'   Default is FALSE.
-#'   
+#'
 #' @returns A downloaded zipfile as specified in `destfile`.
-#' 
-#' 
+#'
+#'
 #'
 
-download_uga_shp <- function(url = "https://data.humdata.org/dataset/6d6d1495-196b-49d0-86b9-dc9022cde8e7/resource/43dbdff6-aadd-4941-957e-f7eab0717f53/download/uga_admbnda_ubos_20200824_shp.zip", 
-  destfile = "data-raw/uganda-shp.zip", 
-  overwrite = FALSE) {
-## Check whether destfile exists and download accordingly ----
-if (file.exists(destfile)) {
-if (overwrite) {
-warning(
-paste0("`", destfile, "` exists and `overwrite = TRUE`. )"),
-paste0("Downloading `", destfile, "`.")
-)
-download.file(url = url, destfile = destfile, mode = "wb")
-} else {
-warning(
-paste0("`", destfile, "` exists and `overwrite = FALSE`. )"),
-paste0("`", destfile, "` will not be downloaded.")
-)
-}
-} else {
-download.file(url = url, destfile = destfile, mode = "wb")
-}
+download_uga_shp <- function(
+    url = "https://data.humdata.org/dataset/6d6d1495-196b-49d0-86b9-dc9022cde8e7/resource/43dbdff6-aadd-4941-957e-f7eab0717f53/download/uga_admbnda_ubos_20200824_shp.zip",
+    destfile = "data-raw/uganda-shp.zip",
+    overwrite = FALSE) {
+  ## Check whether destfile exists and download accordingly ----
+  if (file.exists(destfile)) {
+    if (overwrite) {
+      warning(
+        paste0("`", destfile, "` exists and `overwrite = TRUE`. )"),
+        paste0("Downloading `", destfile, "`.")
+      )
+      download.file(url = url, destfile = destfile, mode = "wb")
+    } else {
+      warning(
+        paste0("`", destfile, "` exists and `overwrite = FALSE`. )"),
+        paste0("`", destfile, "` will not be downloaded.")
+      )
+    }
+  } else {
+    download.file(url = url, destfile = destfile, mode = "wb")
+  }
 
-## Return download path ----
-destfile
+  ## Return download path ----
+  destfile
 }
